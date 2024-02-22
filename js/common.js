d609c1970e351a3c215bd4257d17dc82a14b9376
Mikita Chyzhyk <mikitachyzhyk@gmail.com>
2024-02-22 16:37:49 +0300

    got rid of ru lang

--- js/common.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const urlParams = new URLSearchParams(window.location.search)
-  const currentLang = urlParams.get('lang')
-
-  if ((currentLang && currentLang === 'en') || currentLang === 'ru') {
-    document.documentElement.setAttribute('lang', currentLang)
-  }
-})
